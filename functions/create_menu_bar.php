5b211b98e483cc07f92ebb531adbc63581eaac26
Kain <26943220+kaincenteno@users.noreply.github.com>
2022-05-09 22:01:12 -0700

    link for homepage added

--- functions/create_menu_bar.php
+++ functions/create_menu_bar.php
@@ -1,6 +1,7 @@
 <?php 
 function create_menu_bar(){
     echo '<table class="menubar"><tr>';
+    echo '<td><button onClick="location.href=\'./index.php\'" type="button">Home</button></td>';
     echo '<td><button onClick="location.href=\'https://status.catsangel.com\'" type="button">Server Status</button></td>';
     echo '<td><button onClick="location.href=\'./playersonline.php\'" type="button">Players Currently Online</button></td>';
     echo '<td><button onClick="location.href=\'./weather.php\'" type="button">Weather Forecast</button></td>';
