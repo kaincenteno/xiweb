d1ee7bff72b926fadb851c131157d2952165a5db
Kain <26943220+kaincenteno@users.noreply.github.com>
2022-07-11 19:12:59 -0700

    adds search field to auction house

---
+++ functions/query_auction_house.php
@@ -0,0 +1,46 @@
+<?php 
+function query_auction_house(){
+    require 'config/database.conf';
+    
+    $category = array();
+    $name = array();
+    $stack = array();
+    $listings = array();
+
+    try {
+        $conn = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPass);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $queryAH  = $conn->query('
+            SELECT
+                ib.itemid,
+	            ib.aH,
+	            REPLACE(ib.name, "_", " ") AS "name",
+	            COUNT(*) AS "listings",
+	            (CASE WHEN ah.stack=1 THEN "Y" ELSE "N" END) as "stack"
+            FROM item_basic ib
+            INNER JOIN 
+            	auction_house ah ON ib.itemid = ah.itemid
+            WHERE ah.buyer_name IS NULL
+            GROUP BY ah.itemId, ah.stack
+            ORDER BY ib.aH ASC, ib.itemId;');
+        $i = 0;
+        while ($row = $queryAH->fetch()){
+            if ($row["aH"] == 0) {
+                error_log("Item {$row["name"]} (itemid {$row["itemid"]}) doesnt have a valid category in globals/ahID.php", 0);
+            } else {
+                $category[$i] = $row["aH"];
+                $name[$i] = $row["name"];
+                $stack[$i] = $row["stack"];
+                $listings[$i] = $row["listings"];
+                $i = $i + 1;
+            }
+        }
+
+        return array($category, $name, $stack, $listings);
+    }
+    catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+?>
