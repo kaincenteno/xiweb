81815128f92b0e49342acd86e99f5134b1bffd2d
Kain <26943220+kaincenteno@users.noreply.github.com>
2022-07-24 19:26:16 -0700

    added char list
    if you log in now it greets all of your chars

--- login.php
+++ login.php
@@ -11,25 +11,41 @@ if (strlen($password) > 16) {
    $password = substr($password, 0, 16);
 }
 
+$account = array();
+
 try {
     $conn = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPass);
     // set the PDO error mode to exception+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sth = $conn->prepare('
     SELECT
-        login,
-        password
-    FROM accounts
-    WHERE (login = :username AND password = PASSWORD(:password))');
+        a.login,
+        a.password,
+        c.charname
+    FROM accounts a
+    INNER JOIN
+        chars c ON a.id = c.accid
+    WHERE (a.login = :username AND a.password = PASSWORD(:password))');
     $sth->bindParam(':username', $username, PDO::PARAM_STR);
     $sth->bindParam(':password', $password, PDO::PARAM_STR);
 
     $sth->execute();
-    $result = $sth->fetch(PDO::FETCH_ASSOC);
-    if ($result) {
+    $i = 0;
+    while ($result = $sth->fetch()){
+        $account[$i] = array(
+            "charname" => $result['charname']
+        );
+        $i += 1;
+    }
+    if ($account[0]) {
         session_start();
         $_SESSION['username'] = $username;
-        echo "<p>Welcome, " . $_SESSION['username'] . "</p>";
+        echo "<h1>" . $_SESSION['username'] . "</h1>";
+        echo "<br>";
+        foreach ($account as $char) {
+            echo "Well-met " . $char['charname'];
+        }
+        echo "<br>";
         echo "<p>This is still under construction, but it worked!</p>";
     } else {
         echo "Something went wrong";
