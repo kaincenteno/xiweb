d45ec9d4080b9ff0340d6e37a82724bcb00f0d15
Kain <26943220+kaincenteno@users.noreply.github.com>
2022-11-18 15:41:27 -0800

    format 2 spaces and added content div

--- index.php
+++ index.php
@@ -5,53 +5,53 @@
 
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="xiweb is a website for displaying information from a landsandboat server">
-    <title>Welcome to xiweb</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="icon" type="image/png" href="favicon-32.png" sizes="32x32">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="xiweb is a website for displaying information from a landsandboat server">
+  <title>Welcome to xiweb</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="icon" type="image/png" href="favicon-32.png" sizes="32x32">
 </head>
 
 <body>
+  <div class="content">
+    <div class='menu'></div>
 
-	<div class='menu'></div>
+    <h1>WELCOME TO XIWEB</h1>
 
-	<h1>WELCOME TO XIWEB</h1>
+    <div class="about-us">
+    <h4>ABOUT US</h4>
+      <p>xiweb is a website to show stats from a LandSandBoat server, which is an FFXI emulator.</p>
+    </div>
 
-	<div class="about-us">
-	<h4>ABOUT US</h4>
-		<p>xiweb is a website to show stats from a LandSandBoat server, which is an FFXI emulator.</p>
-	</div>
+    <h4>CLIENT SETUP</h4>
+      <p>Please visit <a href="https://wiki.catsangel.com">Our Wiki</a> to find how to install your client.</p>
 
-	<h4>CLIENT SETUP</h4>
-		<p>Please visit <a href="https://wiki.catsangel.com">Our Wiki</a> to find how to install your client.</p>
+    <h4>DISCORD</h4>
+      <p>You can access it <a href="https://discord.gg/77j69vffNM">here</a></p>
 
-	<h4>DISCORD</h4>
-		<p>You can access it <a href="https://discord.gg/77j69vffNM">here</a></p>
+    <div class="rules">
+    </div>
 
-	<div class="rules">
-	</div>
+    <div class="server-settings">
+    </div>
 
-	<div class="server-settings">
-	</div>
+    <h4>CONTRIBUTE TO XIWEB</h4>
+      <p>You are more than welcome to submit a PR or bug report about typos or new features. This is a hobby
+        project for myself, I am not really good at it but I am more than happy to improve it.
+      </p>
+      <ul>
+        <li><a href="https://github.com/kaincenteno/xiweb">xiweb Github</a></li>
+        <li><a href="https://github.com/kaincenteno/wiki">wiki Github</a>
+      </ul>
 
-	<h4>CONTRIBUTE TO XIWEB</h4>
-		<p>You are more than welcome to submit a PR or bug report about typos or new features. This is a hobby
-			project for myself, I am not really good at it but I am more than happy to improve it.
-		</p>
-		<ul>
-			<li><a href="https://github.com/kaincenteno/xiweb">xiweb Github</a></li>
-			<li><a href="https://github.com/kaincenteno/wiki">wiki Github</a>
-		</ul>
+    <h4>CONTRIBUTE UPSTREAM(LANDSANDBOAT REPO)</h4>
+      <p>Upstream has high standards on the code being merged. PLEASE PLEASE PLEASE, check past PRs and read their
+        guidelines before submitting any work. We do not want anyone's feelings hurt, and/or their time wasted.
+      </p>
 
-	<h4>CONTRIBUTE UPSTREAM(LANDSANDBOAT REPO)</h4>
-		<p>Upstream has high standards on the code being merged. PLEASE PLEASE PLEASE, check past PRs and read their
-			guidelines before submitting any work. We do not want anyone's feelings hurt, and/or their time wasted.
-		</p>
-
-		<ul>
-			<li><a href="https://github.com/LandSandBoat/server">LandSandBoat Server Github</a></li>
-		</ul>
-
+      <ul>
+        <li><a href="https://github.com/LandSandBoat/server">LandSandBoat Server Github</a></li>
+      </ul>
+  </div>
 </body>
