42df7f2d1bd135be70cc5c0b7ee11668c7a6f3d3
Kain <26943220+kaincenteno@users.noreply.github.com>
2023-04-04 19:21:38 -0700

    adds spanish translation to index as a test (#35)
    * adds spanish translation to index as a test
    
    * fixes complaint from sonarcloud

--- index.php
+++ index.php
@@ -17,56 +17,24 @@
   <div class="content">
     <div class='menu'></div>
 
-    <h1>WELCOME TO xiweb</h1>
+    <div class='welcome'></div>
 
-    <div class="about-us">
-    <h4>ABOUT US</h4>
-      <p>xiweb is a website to show stats from a LandSandBoat server, which is an FFXI emulator.</p>
-    </div>
+    <div class="about-us"></div>
 
-    <h4>CLIENT SETUP</h4>
-      <p>Please visit <a href="https://wiki.catsangel.com">Our Wiki</a> to find how to install your client.</p>
+    <div class="client-setup"></div>
 
-    <h4>DISCORD</h4>
-      <p>You can access it <a href="https://discord.gg/77j69vffNM">here</a></p>
+    <div class="discord"></div>
 
-    <div class="rules">
-    </div>
+    <div class="rules"></div>
 
-    <div class="server-settings">
-    </div>
+    <div class="server-settings"></div>
 
-    <h4>CONTRIBUTE TO XIWEB</h4>
-      <p>You are more than welcome to submit a PR or bug report about typos or new features. This is a hobby
-        project for myself, I am not really good at it but I am more than happy to improve it.
-      </p>
-      <ul>
-        <li><a href="https://github.com/kaincenteno/xiweb">xiweb Github</a></li>
-        <li><a href="https://github.com/kaincenteno/wiki">wiki Github</a>
-      </ul>
+    <div class="contribute"></div>
 
-    <h4>FOUND A BUG IN THE GAME</h4>
-      <p>We encourage you to submit it through a discussion. If you have found a bug in the game:
-      </p>
-      <ul>
-        <li><a href="https://github.com/LandSandBoat/server/discussions">LandSandBoat Server Github Discussion</a></li>
-      </ul>
+    <div class="found-bug"></div>
 
-    <h4>RUN YOUR OWN SERVER</h4>
-      <p>You can find all the information needed to run your own private server by going to the project's
-        github wiki. We (xiweb) do not provide support for setting up your own server.
-      </p>
-      <ul>
-        <li><a href="https://github.com/LandSandBoat/server/wiki">LandSandBoat Server Github Wiki</a></li>
-      </ul>
+    <div class="your-own-server"></div>
 
-    <h4>CONTRIBUTE UPSTREAM (LANDSANDBOAT REPO)</h4>
-      <p>Upstream has high standards on the code being merged. PLEASE PLEASE PLEASE check past PRs and read their
-        guidelines before submitting any work. We do not want anyone's feelings hurt and/or their time wasted.
-      </p>
-
-      <ul>
-        <li><a href="https://github.com/LandSandBoat/server">LandSandBoat Server Github</a></li>
-      </ul>
+    <div class="contribute-upstream"></div>
   </div>
 </body>
