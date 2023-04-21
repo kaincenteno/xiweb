d963f2c096240b6cb414213139d0acff4477c407
Kain Zhou <kain.centeno@catsangel.com>
2023-04-21 15:23:21 -0700

    uses rootUrl var to specify root of site

--- script/menu.js
+++ script/menu.js
@@ -1,14 +1,17 @@
 function createMenu() {
     let tr = document.querySelector(".menu > table > tbody > tr")
+    let rootUrl = window.location.origin
+
+
 
     const menuItems = [
-        "<button onClick=\"location.href='./index.php'\" type=\"button\">Home</button>",
+        "<button onClick=\"location.href='" + rootUrl + "/index.php'\" type=\"button\">Home</button>",
         "<button onClick=\"location.href='https://status.catsangel.com'\" type=\"button\">Server Status</button>",
-        "<button onClick=\"location.href='./playersonline.php'\" type=\"button\">Players Currently Online</button>",
-        "<button onClick=\"location.href='./weather.php'\" type=\"button\">Weather Forecast</button>",
-        "<button onClick=\"location.href='./auctionhouse'\" type=\"button\">Auction House</button>",
-        "<button onClick=\"location.href='./item_drop.php'\" type=\"button\">Item Drop</button>",
-        "<button onClick=\"location.href='./my_account.php'\" type=\"button\">My Account</button>",
+        "<button onClick=\"location.href='" + rootUrl + "/playersonline.php'\" type=\"button\">Players Currently Online</button>",
+        "<button onClick=\"location.href='" + rootUrl + "/weather.php'\" type=\"button\">Weather Forecast</button>",
+        "<button onClick=\"location.href='" + rootUrl + "/auctionhouse'\" type=\"button\">Auction House</button>",
+        "<button onClick=\"location.href='" + rootUrl + "/item_drop.php'\" type=\"button\">Item Drop</button>",
+        "<button onClick=\"location.href='" + rootUrl + "/my_account.php'\" type=\"button\">My Account</button>",
     ]
 
     menuItems.forEach((item) => {
