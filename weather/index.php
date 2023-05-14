73f592f731e69f4c46a0aab9248aa3fd6a7d500d
Kain Zhou <kain.centeno@catsangel.com>
2023-05-14 15:36:07 -0700

    moves logic from weather to php and js files

--- weather/index.php
+++ weather/index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-require 'php/create_weather_table.php';
 header("Refresh: 142"); // One vanadiel hour(ish)
 ?>
-<script async type="text/javascript" src="../script/menu.js"></script>
 
 <head>
   <meta charset="utf-8">
@@ -24,6 +22,22 @@ header("Refresh: 142"); // One vanadiel hour(ish)
         </tbody>
       </table>
     </div>
-  <?php create_weather_table();?>
+
+    <div class="weather">
+      <h3 class="vanadielTime"></h3>
+      <h4>The weather forecast for tomorrow is as follows.</h4>
+      <table class="plaintable">
+        <thead>
+          <tr><th>Zone Name</th><th>Likely 50%</th><th>Chance 35%</th><th>Rarely 15%</th></tr>
+        </thead>
+        <tbody class="weather_body">
+        </tbody>
+      </table>
+    </div>
+
   </div>
+
+  <script type="text/javascript" src="../script/menu.js"></script>
+  <script type="text/javascript" src="/weather/script/vanadiel_time.js"></script>
+  <script type="text/javascript" src="/weather/script/query_zone_weather.js"></script>
 </body>
---
+++ weather/php/queryZoneWeather.php
@@ -0,0 +1,97 @@
+<?php
+    function VanadielClock(){
+        $VANA_EPOCH     = 1009810800;
+        $VANA_YEAR      = 518400;
+        $VANA_MONTH     = 43200;
+        $VANA_DAY       = 1440;
+        $VANA_HOUR      = 60;
+
+        $vanadielNow = time() - $VANA_EPOCH;
+        // Total elapsed vanadiel minutes ever
+        $vanaTimestamp = ($vanadielNow / 60.0 * 25) + 886 * $VANA_YEAR;
+
+        return array(
+            'year'            => (int)($vanaTimestamp / $VANA_YEAR),
+            'month'           => ((int)($vanaTimestamp / $VANA_MONTH) % 12) + 1,
+            'day'             => ((int)($vanaTimestamp / $VANA_DAY) % 30) + 1,
+            'hour'            => (int)(((int)$vanaTimestamp % $VANA_DAY) / $VANA_HOUR),
+            'minute'          => (int)$vanaTimestamp % $VANA_HOUR,
+            'vana_now'        => $vanadielNow,
+            'vana_timestamp'  => $vanaTimestamp,
+        );
+    }
+
+    // The count on the 2160 day cycle
+    function WeatherCycleDay($vanatime)
+    {
+        return floor($vanatime['vana_now'] / 3456) % 2160;
+    }
+
+    function GetWeatherForDay($blob, $day)
+    {
+        // (PHP unpacked indexes start at 1... so day 0 = index 1.)
+        $day += 1;
+        $blob = unpack("v*", $blob);
+        if($blob[$day] >> 15){
+            echo "Encountered non-zero padding bit. Possible encoding or DB error.";
+        }
+        // Decode weather blob
+        return array(
+            "rare"   => $blob[$day] & 31,
+            "common" => ($blob[$day] >> 5)  & 31,
+            "normal" => ($blob[$day] >> 10) & 31
+        );
+    }
+
+    require $_SERVER['DOCUMENT_ROOT'] . '/config/database.conf';
+    require $_SERVER['DOCUMENT_ROOT'] . '/globals/weatherID.php';
+    
+    $vanatime = VanadielClock();
+    $today = WeatherCycleDay($vanatime);
+    $tomorrow   = ($today + 1) % 2160;
+    $zones = array();
+    $output = array();
+
+    try {
+        $conn = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPass);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $queryWeather  = $conn->query('
+        SELECT
+            w.zone,
+            w.weather,
+            REPLACE(z.name,"_"," ") name
+        FROM zone_weather w
+        LEFT JOIN zone_settings z ON z.zoneid = w.zone
+        WHERE CAST(w.zone AS char) <> z.name
+            AND z.name NOT LIKE "Abyssea%"
+            AND z.name NOT LIKE "unknown"
+        ORDER BY z.name;');
+        $z = 0;
+        while ($row = $queryWeather->fetch()) {
+            // Add entry for this zone
+            $zones[$z] = array(
+                "name" => $row["name"],
+                "id" => $row["zone"],
+                "weather" => $row['weather']
+            );
+            $z += 1;
+        }
+
+        for ($i = 0; $i < count($zones) ; $i++){
+            $zone = $zones[$i];
+            $forecast = GetWeatherForDay($zone["weather"], $tomorrow);
+
+            $zoneName = $zone["name"];
+            $normalWeather = $weatherID[$forecast["normal"]];
+            $commonWeather = $weatherID[$forecast["common"]];
+            $rareWeather = $weatherID[$forecast["rare"]];
+
+            $output[$i] = [$zoneName, $normalWeather, $commonWeather, $rareWeather];
+        }
+
+        echo json_encode($output);
+
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
