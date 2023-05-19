10fa15857c1d02df292b6eba0dbddc8011b16ba9
Kain <26943220+kaincenteno@users.noreply.github.com>
2023-05-19 14:26:12 -0700

    sonarlint was complaining of missing let

--- script/index.js
+++ script/index.js
@@ -5,11 +5,11 @@ async function get_language_data() {
     const response = await fetch('./script/language/index.json')
     const language_data = await response.json()
 
-    for(languageId in language_data) {
+    for(let languageId in language_data) {
         if (["zh", "es"].includes(browser_language)) {
             console.log(language_data[languageId])
             console.log(language_data[languageId][browser_language])
-            idTag = document.getElementById(language_data[languageId])
+            let idTag = document.getElementById(language_data[languageId])
             idTag.textContent = language_data[languageId][browser_language]
         } else {
             console.log(languageId)
